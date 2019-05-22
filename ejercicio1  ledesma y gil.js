59fa59be6c25f495e4f1c0bddc835eb855c1f315
brantu <gilferioli.martina@gmail.com>
2019-05-22 09:19:38 -0300

    Update ejercicio1  ledesma y gil.js

--- ejercicio1  ledesma y gil.js
+++ ejercicio1  ledesma y gil.js
@@ -1,4 +1,5 @@
 var=secretnumber=8
 pompt(secretnumber)
-if8(secretnumber==1){"correct"}false
-
+if(secretnumber==8){console.log("correct")}else{incorrect}
+if(secretnumber<8) console.log("incorrect")
+if(secretnumber>8) console.log("incorrect")
